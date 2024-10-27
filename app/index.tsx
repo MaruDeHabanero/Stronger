2e19786804dab089682e28bc9a73c4d3b62b80fe
MaruDeHabanero <jose13.maciasm@gmail.com>
2024-10-26 22:51:07 -0600

    moviendole al Flexbox, commit feo

--- app/index.tsx
+++ app/index.tsx
@@ -4,20 +4,35 @@ import { Colors } from "react-native/Libraries/NewAppScreen";
 export default function Index() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Stronger mamei.</Text>
+      <View style={styles.miniContainer}>
+        <Text style={styles.text}>Hello World</Text>
+      </View>
+      <View style={styles.miniContainer}>
+        <Text style={styles.text}>Hello World, but 2</Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    display: "flex",
+    flexDirection: "column",
     justifyContent: "center",
     alignItems: "center",
     backgroundColor: "#25292e",
+    height: "100%",
   },
   text: {
     fontSize: 50,
     color: "white",
-  }
+  },
+  miniContainer: { 
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    borderWidth: 10,
+    borderColor: "red",
+    width: "100%",
+  },
 });
